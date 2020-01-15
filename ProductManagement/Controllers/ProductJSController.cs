8c4a53cf6b4da294d653cead4a10fba63d9d2c16
phí kiên <44530636+PhiKien@users.noreply.github.com>
2020-01-15 22:00:11 +0700

    ok

---
+++ ProductManagement/Controllers/ProductJSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagement.Controllers
+{
+    public class ProductJSController : Controller
+    {
+        // GET: ProductJS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
