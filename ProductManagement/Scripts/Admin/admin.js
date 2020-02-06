eee4ab2542bde3625770ebbed57abd2e4c00f04f
phí kiên <44530636+PhiKien@users.noreply.github.com>
2020-02-07 00:05:20 +0700

    7/2/2020

---
+++ ProductManagement/Scripts/Admin/admin.js
@@ -0,0 +1,23 @@
+﻿var admin = function () {
+
+    this.init = function () {
+        //loadData();
+        registerEvents();
+    }
+
+    var registerEvents = function () {
+
+        $('body').on('click', '#btn-product-admin', function (e) {
+            e.preventDefault();
+           
+            var a = document.getElementById("btn-link");
+
+            a.innerHTML = 'Go to Users';
+            setInterval(function () { a.href = '/Admin/Users'; }, 2000); 
+            
+            console.log('hello!');
+        });
+
+        
+    }
+}
